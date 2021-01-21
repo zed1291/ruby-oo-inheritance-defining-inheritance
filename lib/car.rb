17a857aa38e75f6228d6ac930fadaf68fb3e2173
“zed” <zed@kolk.me>
2021-01-21 14:28:14 -0600

    commit tree 70c3533ef6dfd9f71cb93648bfd2c3b6b11ecbe6

--- lib/car.rb
+++ lib/car.rb
@@ -1,1 +1,15 @@
-require_relative "./vehicle.rb"
+class Car
+    attr_accessor :wheel_size, :wheel_number
+    def initialize wheel_size, wheel_number
+        @wheel_size = wheel_size
+        @wheel_number = wheel_number
+    end
+    def go
+        'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!'
+    end
+
+    def fill_up_tank
+        'filling up!'
+    end
+
+end
--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,8 @@
-class Vehicle
+require_relative "./car.rb"
+class Vehicle < Car
+
+    def go
+        'vrrrrrrrooom!'
+    end
 
 end
